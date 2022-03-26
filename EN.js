a52ffb710edd2e27f8dc32eec3d0cab634a1be8d
LZY <luzhengyond1@outloom.com>
2022-03-26 15:43:19 +0800

    3/26

--- EN.js
+++ EN.js
@@ -1,4 +1,69 @@
 /**
 material  材料  原料
 fabric   布料  织物
+
+*** shoping online 
+tops  上装
+jumpers 连体装
+*? search for the best price
+when you shop on line, you can search for the best price
+*? read online review 
+you can read review from other people who bought the same thing
+add it to your basket
+*? receive the delivery  / wait for a delivery
+*? order number   color(美)  colour(英)
+
+*** in a hotel
+reception 接待处
+counter   柜台
+
+luggage baggage  均指 行李 的意思
+suitcase         特指 手提行李箱  
+I have a reservation [I booked a room]  预定
+
+can I check out, please?  结账
+can I have the bill, please?  结账
+can I have wake-up call at 6:30, please?
+
+***eating  out
+take-away  外带 
+fast food restaurant 快餐
+dessert  （餐后）甜点
+Are you ready to order?
+how would you like your steak - rare,medium, well-done
+
+*** sport
+ do you do any sports?
+rugby 英式橄榄球
+American  football 美式橄榄球
+
+motor  racing  赛车
+kayaking  皮筏艇 独木舟
+
+skiing  两个滑雪板和滑雪杆
+snowboarding  一个滑雪板
+
+*? play...  go...  do...
+play  footbal  baskateball...
+go    swimming  running...
+do    judo / karate...  柔道 / 空手道
+
+pitch 球场    
+football pitch  足球场
+
+*** cinema
+horror film / fiction  恐怖片 / 小说
+thriller  film  惊悚片
+comedy  film  喜剧片
+romantic  comedy  film 爱情喜剧片
+action  film  动作电影
+science  fiction  科幻小说
+a musical   歌舞剧
+
+what's on at the cinema  this week?
+there is a comedy with ZhouxingChi
+
+***  free  time  at  home
+
+
 */
