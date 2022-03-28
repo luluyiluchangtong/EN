095068d901e762c356fe23093b8648d6aaebbd5d
LZY <luzhengyond1@outloom.com>
2022-03-29 07:16:53 +0800

    3/29

--- EN.js
+++ EN.js
@@ -59,11 +59,145 @@ romantic  comedy  film 爱情喜剧片
 action  film  动作电影
 science  fiction  科幻小说
 a musical   歌舞剧
+*? the cinema
 
-what's on at the cinema  this week?
+*? what's on at the cinema  this week?
 there is a comedy with ZhouxingChi
 
 ***  free  time  at  home
+programme 项目  计划  节目  方案
+at  the weekend  在周末
+I like listening to music / I like to listhen  music
+headphone  耳机 听筒
+I chat to my friends online / on the phone ever evening 
+my best friend  comes to stay  sometimes  留宿
+I often have friends to dinner
+
+*** music and musical instrument
+*? plural  复数
+   singular  单数
+cello 大提琴
+violin  小提琴
+
+flute  长笛
+clarinet  单簧管
+
+trumpet  小号
+*? drum  鼓  圆桶
+drum tower  鼓楼
+iron drum 铁桶  oil  drum  油桶
+
+orchestra 管弦乐团
+symphony  交响乐团
+
+Emily is having a piano lesson
+
+*** countries  and nationalities
+the Arctic Ocean  北冰洋
+Antarctica        南极洲
+
+*** weather
+45 degrees / minus  50 degrees   45度  零下 50度
+go  sightseeing  观光  旅游
+hurricane  飓风
+tornado   龙卷风
+what's the weather like in you country in june
+
+***  in the town
+Bus station --- you can get a bus here
+*? pedestrian  行人  步行的
+pedestrian  street  步行街
+Museum --- you can see intresting  old things  here
+shopping center ---  area of town with a lot of shops   town 城镇  中心区
+
+How do I get to Market Street?
+Pleace keep off the grass
+tourist  information  office  旅游咨询处
+network out of order  网络故障
+*? out of order 发生故障  秩序混乱
+
+*** in the countryside
+mountain VS hill  高山 和 山丘
+forest  VS  wood  树林 和 树木
+*? cottage  小屋  乡村别墅
+My friend lives in a cottage(小屋  乡村别墅)
+I love nature, I like walking in the countryside.
+A big national conservation area  一个大型自然保护区
+have a picnic  野餐
+country  road  VS  path  乡村道路 和 羊肠小道
+small town(6000 people)  VS  village( 700 people)
+
+*** Animals
+cock  公鸡   hen  母鸡   chicken  鸡  鸡肉
+各个动物的幼崽称呼：
+*? horse VS foal；   小马驹
+*? cow VS calf；     小牛犊
+*? sheep  VS lamb；  小羊羔
+*? goat  VS  kid；   小山羊
+*? pig  VS piglet；  小猪仔
+*? chicken  VS  chick； 小鸡仔
+
+pet  宠物
+tortoise  乌龟
+crossword  纵横字谜
+
+*** Travelling
+timetable  schedule  时刻表  课程表
+前者是日复一日不断重复的事情， 后者一般为短期 或 一次性的事情；
+single or return ticket to Huangshan.  单程票  往返票
+platform  平台 月台
+restaurant  car  餐车
+Do i have to change trains for hangzhou?
+No, It's a direct train.
+The train arriving at platform 3      is the 16:50 trian to Paris.
+
+attendant  陪同  随从
+waiter  专职服务员
+go through custom  过海关
+
+*? land
+The plane lands in New York at 14:30
+
+*? fill up
+fill up the car with petrol  给小汽车加满油
+
+lift  电梯 举起 搭便车
+can I give you al lift 顺便载你一程
+
+*? take a number 10 bus
+when you arrive at the airport, take a number 10 bus to the station.
+
+*** UK  cultrue
+valentine  情人节
+easter   复活节
+halloween   万圣节
+bonfire night  篝火节
+firework  烟花  烟火
+firework  display   放烟花
+roast  烘烤  oven  烤炉  烤箱
+state  school  VS  private  school  公立学校 和  私立学校
+nursery  school  托儿所  幼儿园  kindergarten   infants' school
+New years  day  VS  new year's  Eve
+a bank holiday  法定假日
+Houses  of Parliament  国会大厦
+
+*** Crime
+steal  盗窃
+burglary  入室盗窃  burglar
+thief  贼  car theft
+shoplifting  商店行窃    shoplifter 商店扒手
+
+rob 抢劫  robber     robbery
+mug  马克杯  （公开）行凶抢劫  mugger    mugging
+
+murder  谋杀  murderer   murder
+vandal 故意破坏公共财产者
+vandalism  故意破坏公共财产的行为
+hooliganism  流氓行为  football  hooliganism
+
+
+
+
 
 
 */
