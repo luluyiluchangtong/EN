8f71dfd4ed310727f7052e4fd2ed0a6a3b6ad42d
LZY <luzhengyond1@outloom.com>
2022-04-15 06:42:49 +0800

    4/15

--- EN-2.js
+++ EN-2.js
@@ -1,4 +1,53 @@
 /**
 *** learning  vocabulary
+review  评论  复习
+revise  修订  复习
+bilingual  dictionary   双语词典
+filthy  及其肮脏的  dirty --> filthy
+死了的， 活着的： dead  alive
+
+图表可能会帮助你组织一些单词的不同之处
+diagrames may help you to organise some of the vocabulary differently
+
+你穿什么 尺码的鞋子
+what size shoes do you take?
+
+当你学习一个单词，想一想在自己的语言里它的同义词或反义词？
+When you learn a new word think of synonyms or opposites in your language
+
+在英文字典里找出这个单词 并检查他的意思
+Look up the english words / in an english dictionary / and check the meaning
+
+使用词典找出与黑体字部分读音相关的词类
+Use a dictionary to find / the related / parts of speech for the words in bold
+
+make / take a note of
+记下他们的名字
+take a note of their names and address.
+
+请注意牢记他们说的话 
+take a note of what he says. 
+
+as well  也  同样
+他们也来吗？
+Are they coming as well? 
+她玩的没有他姐姐一样好
+She doesn't play as well as her sister.
+
+I note down / make a note of new words and phrases.
+A tip is a piece of advice for you, and is also money that you give
+raw  生的  未加工的
+lay the  table （饭前）摆好餐具
+dig  搜寻  挖苦
+flour  面粉
+
+一本英语字典，一本双语字典
+A bilingual dictionary,  A dictionary in English
+most of dictionaries are avaliable online
+
+phonemic symbol  音标
+The phonemic of *fun* is  /fʌn/.
+The bold italics *Have fun* show / common word partners  常见的单词搭配
+/fixed phrases / using the word are shown in bold (make fun of)  固定短语搭配  
 
 */
