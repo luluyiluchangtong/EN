c5aa2dceacf22b53968094c6f68fcd5d4ea89447
LZY <luzhengyond1@outloom.com>
2022-06-14 05:54:36 +0800

    6/14

--- EN-3.js
+++ EN-3.js
@@ -19,6 +19,7 @@ irregular verb (teach  read  write  put)
 a `record / re`cord
 音标 phonetic  音节 syllable   重音 stressed  syllable
 词性 parts of speech: noun, verb, adverb, adjective,
+词类：word of class  分类标准可以自定义
 
 pop-up window   弹出式窗口
 synonyms and antonyms  同义词  反义词
@@ -44,6 +45,134 @@ she laughed when I told her the news.
 thesaurus 词典 索引典
 thermometer  温度计  体温表
 extract  提取 提取物   palnt extract 植物提取物  extract  from
+
+*? clue  线索  提示  get a clue
+those clues to /make sense of/ unfamiliar words.
+resemble  像， 与.... 相似
+come across 偶遇 无意中发现    
+you will /come across/ many words in english that /resemble words/ in your own language.
+*? work out 解决   
+it is easy to work out that....； use the context to work out what the undelined words mean.
+*? reptile  爬行动物 tile == tail   mammal 哺乳动物
+*? longevity  寿命， 长寿；  lifespan 寿命； hibernate 冬眠；
+*? obtain  attain  获得:
+*? 前者是（具体的） 付出努力得到（get）某物的所有权；后者是（抽象的）通过努力完成 或 达到某事,某个目标
+grassland  meadow  草地 草坪
+前者大都是指野外的草地，比较杂乱。 后者大多与农业家禽有关， 比较矮。
+extinction  灭绝  消亡；  mess extinction 大量消亡  fire extinction  灭火
+water vole 田鼠
+凿子和锤子： chisel  and  hammer
+tureen  汤碗  汤锅    soup tureen
+cliff  悬崖  cut + lift 砍掉电梯，升降机 ==》 悬崖
+
+he has a heart of gold
+he's as good as gold
+
+she's as hard as nails
+she's a nasty piece of work
+
+she's a pain in the neck   令人讨厌的人或事情
+He gets on everyone’s nerves  令人心烦的人
+she's an pain in the neck. so, when she go shpping,
+she's such an awkward customer. and get on everyone's nerve.
+but the waiter's heart in the right place.
+
+*? head:
+to have you head....
+clouds(not reality)  screw(sensible)  
+to have a head....
+for figure(good at maths)  like a sieve(筛网 bad memory)   
+for height( to be able to be in a high position without being afraid or feeling)
+
+to be head and shouders above someone  (much better than)
+bury your head in the sand;   keep your head  (stay calm)
+
+an-odd-ball (strange)   over the top (exaggerate[ 夸大的，言过其实的] in behavior)  
+round the bend(mad,crazy)  middle-of-the-road(中立)
+a-big head 有点自大   teacher's pet (teacher's favourite student)
+
+*? 漏水， 打碎， 裂缝，  chip 薯条 芯片 碎块， 停电，压碎，故障
+leak， smash，crack，                 power out，crush，crash
+The pipe's leaking;
+smashed  a  window;
+The pot is cracked
+*? 打碎  压碎  裂缝  smash crush crack
+*? brake down； crash
+door handle  fallen off  门把手掉了  
+washing mathine brake down;  computer crash;
+twist your wist  扭弯、扭动 你的 腰
+*? bump  碰上 撞上 肿块;       bruise 青淤 伤痕 
+bump your head and got a bruise；  a bump in the road
+*? trip 旅行  绊倒   grase 放牧  擦伤
+Kelly tripped and fell down and grased her knee 
+
+mislaid  miss + lay  放错  遗失
+spilt 洒出  溢出
+I spilt some coffee on the carpet
+*? dent  使产生凹痕， 凹痕
+I'm afraid I've dented your car.
+车子启动不了了： The car won't start.  battery's flat.
+flat 平的 公寓 没电的
+钟快了，慢了，不走了： clock's slow/fast/stopped
+
+*! bump -》bruise  清淤 伤痕；  tripped -》grase 放牧 擦伤； spilt 洒出 溢出-》 stain； 
+
+plumb 垂直的 装水管    plumber 水管工；  sink -》 blocked 水槽堵住了
+batteries   have run out / are dead / flat  电池的电用完了
+light -》 power  cut  停电了
+handle-》 fallen off； someboday-》 fallen down；
+*? matrix  模型  矩阵
+burnt 烧坏的  烧焦的
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
